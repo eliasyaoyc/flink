c2fffc0de7196f76918d873f8f18229ca216fb0a
Elias.Yao <siran0611@gmail.com>
2022-12-22 12:02:06 +0800

    [yaofuck]: demo module

---
+++ yaofuck/src/main/java/org/apache/flink/Main.java
@@ -0,0 +1,10 @@
+package org.apache.flink;
+
+/**
+ * @author Elias (siran0611@gmail.com)
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
